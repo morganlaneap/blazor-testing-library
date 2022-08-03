1fa7935c40a10eca10f77e1fcefd6aa78ced67d5
Morgan Lane <morgan.lane@outlook.com>
2022-08-03 15:53:51 +0100

    Create project with FindByText

---
+++ BlazorTestingLibrary/BtlExtensions.cs
@@ -0,0 +1,25 @@
+﻿using AngleSharp.Dom;
+using Bunit;
+
+namespace BlazorTestingLibrary;
+
+public static class BtlExtensions
+{
+    public static IElement? FindByText(this IRenderedFragment renderedFragment, string text)
+    {
+        INode? SearchNodes (INodeList nodeList)
+        {
+            foreach (var node in nodeList)
+            {
+                if (node.HasChildNodes)
+                {
+                    var result = SearchNodes(node.ChildNodes);
+                    if (result != null) return result;
+                }
+                if (node.TextContent == text) return node; 
+            }
+            return null;
+        }
+        return SearchNodes(renderedFragment.Nodes)?.ParentElement;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # blazor-testing-library
 A (better) React-like testing approach for Blazor apps
+
+### FindByText
+Searches for a particular text string in the DOM and returns the first element that contains it.
